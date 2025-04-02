90ceefa8ba7bc6f2b427e571edd150eac523d157
sweet.dh <dlsrksrhk@gmail.com>
2025-04-02 18:42:55 +0900

    버그 수정 및 최종 소스 커밋

---
+++ server/src/main/java/com/example/demo/config/MySQLConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import jakarta.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class MySQLConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource datasource) {
+        return new DataSourceTransactionManager(datasource);
+    }
+
+    @Bean(name ="createChatTransacationMansger")
+    public PlatformTransactionManager createChatTransacationMansger(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Bean(name = "createUserTranscationManager")
+    public PlatformTransactionManager createUserTranscationManager(DataSource datasource) {
+        DataSourceTransactionManager manager = new DataSourceTransactionManager(datasource);
+        return manager;
+    }
+}
